1d03609f2001cb47a3c72874da2641f4b86a173d
ramivo08 <hboram8@naver.com>
2023-01-18 02:46:42 +0900

    Update 짝수의 합.java

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -1,9 +1,9 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-            for(int i =0; i <=n; i++){
-                if(i % 2 == 0){
-                    answer += i;
+            for(int i =0; i <=n; i++){ //n(주어진 횟수)만큼 반복하고
+                if(i % 2 == 0){ //짝수일때
+                    answer += i; //i를 answer에 누적시킨다.
                 }
             }
         return answer;
