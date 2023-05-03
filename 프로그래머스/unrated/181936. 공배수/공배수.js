bceefde7f2746442f66f005040bca2a0b41fb23b
ramivo08 <hboram8@naver.com>
2023-05-04 01:15:58 +0900

    [unrated] Title: 공배수, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181936. 공배수/공배수.js
@@ -0,0 +1,11 @@
+function solution(number, n, m) {
+    var answer = 0;
+    
+    if(number% n == 0 && number% m == 0){
+        return 1;
+    }else{
+        return 0;
+    }
+    
+    return answer;
+}
