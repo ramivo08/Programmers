118dfd4df9e93a9300945179338e07d6732f3874
ramivo08 <hboram8@naver.com>
2023-05-07 23:54:13 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,14 +1,14 @@
 function solution(num_list) {
-    var answer = [];
-    var even = [];
-    
-    for(var i=0; i<num_list.length; i++){
-        if(num_list[i]%2==0){
-           even.push(i);//ㅑ번째 원소? 짝수 배열에 넣기
+    var answer = [0,0];
+
+    for(var i=0 ; i<num_list.length; i++){
+        if(num_list[i] % 2 == 0) {
+            answer[0]++;
         }
-           
+        else{
+            answer[1]++;
+        }
     }
-    
-    return answer=[even.length, num_list.length - even.length];
-    //짝수개수, 정수가 담긴 num_list.length - 짝수 even.length
+
+    return answer;
 }
