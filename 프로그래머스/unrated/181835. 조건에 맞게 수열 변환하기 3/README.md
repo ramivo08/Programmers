f5457e102064e4d63e3737a315f7815f0893fc49
ramivo08 <hboram8@naver.com>
2023-05-09 01:43:29 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 3, Time: 29.46 ms, Memory: 45.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/README.md
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/README.md
@@ -1,10 +1,10 @@
 # [unrated] 조건에 맞게 수열 변환하기 3 - 181835 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181835) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181835?language=javascript) 
 
 ### 성능 요약
 
-메모리: 45.3 MB, 시간: 5.11 ms
+메모리: 45.3 MB, 시간: 29.46 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
@@ -4,9 +4,9 @@ function solution(arr, k) {
     for(var i=0; i<arr.length; i++){
         if(k % 2 != 0){
            
-           answer.push(arr[i] *= k);
+           answer.push(arr[i] * k);
         }else{
-            answer.push(arr[i] += k);
+            answer.push(arr[i] + k);
         }
            
     }
