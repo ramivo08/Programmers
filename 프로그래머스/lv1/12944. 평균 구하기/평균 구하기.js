35464bcd793f44615f6677b36ce49d8e413530af
ramivo08 <hboram8@naver.com>
2023-05-10 00:25:59 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
@@ -1,12 +1,24 @@
 function solution(arr) {
     var sum =0;
-    var avg=0;
     
     for(var i=0; i<arr.length; i++){   
         sum += arr[i]
     }
-     avg = sum / arr.length;  
-    console.log(avg);
     
-    return avg;
+    return  sum / arr.length;  
 }
+
+
+
+//function solution(arr) {
+//    var sum =0;
+//    var avg=0;
+    
+//    for(var i=0; i<arr.length; i++){   
+//        sum += arr[i]
+//    }
+ //    avg = sum / arr.length;  
+///    console.log(avg);
+    
+//    return avg;
+//}
