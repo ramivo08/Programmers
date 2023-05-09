0f6aaf77f2f90757fdf38ad256a62f418a1dd2bc
ramivo08 <hboram8@naver.com>
2023-05-10 00:16:05 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -3,8 +3,21 @@ function solution(array, n) {
     
    for(var i=0; i<array.length; i++){
         if(array[i] === n){
-            arr.push(array[i]);
+           arr.push(array[i]);
         }
     }
     return arr.length;
 }
+
+
+
+//function solution(array, n) {
+//    var answer = 0;
+    
+//   for(var i=0; i<array.length; i++){
+//        if(array[i] === n){
+//           answer++;
+//        }
+//    }
+//    return answer;
+//}
