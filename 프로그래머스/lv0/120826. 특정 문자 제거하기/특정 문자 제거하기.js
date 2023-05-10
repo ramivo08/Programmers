7fa5d7c259d9bccc2b5ab4c6ba46c1c33e8430cd
ramivo08 <hboram8@naver.com>
2023-05-11 01:14:12 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -0,0 +1,15 @@
+function solution(my_string, letter) {
+  //  var answer = '';
+   // console.log(my_string);
+   // console.log(letter);
+    
+ //   for(var i=0; i<my_string.length; i++){
+ //       if(my_string[i]===letter){
+  //        my_string.replace(letter,'');
+  //      }
+       
+ //   }
+    return my_string.replaceAll(letter,"");
+}
+
+
