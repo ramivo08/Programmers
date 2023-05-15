5c8e10f4999469ba8bc46846fbc773da50e21791
ramivo08 <hboram8@naver.com>
2023-05-16 01:21:36 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,7 +1,7 @@
 function solution(a, b) {
     var sum = 0;
-    
-    if(a<=b){
+    //a와 b의 대소관계가 정해져 있지 않아서 조건문으로 분기문을 작성해 보았다.
+    if(a<=b){//조건에 따라서 for문의 시작과 끝이 달라지며, sum에 누적되게 해보았다.
         for(var i=a; i<=b; i++){
             sum += i;
         }
