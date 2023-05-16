34c9e782c5ce0290d50684b1773e7accac940d06
ramivo08 <hboram8@naver.com>
2023-05-17 00:55:12 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -20,3 +20,15 @@ function solution(num) {
     
     return answer;
 }
+
+
+
+//다른 사람 풀이
+//function collatz(num) {
+//    var answer = 0;
+//    while(num !=1 && answer !=500){
+//        num%2==0 ? num = num/2 : num = num*3 +1;
+//    answer++;
+//  }
+//    return num == 1 ? answer : -1;
+//}
