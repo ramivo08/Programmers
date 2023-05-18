e67a9ccdc6f51189e69fb3404a56c77586bc05b6
ramivo08 <hboram8@naver.com>
2023-05-18 23:41:11 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,11 +1,17 @@
 function solution(seoul) {
+    //틀렸던이유: Kim 을 소문자로 썼기때문..
+//    var answer = '';
     
-    var answer = '';
+//    for(var i=0; i<seoul.length; i++){
+ //      if(seoul[i] === 'Kim') {
+ //          answer = "김서방은 " +i+ "에 있다";
+ //      }
+ //   }
+ //   return answer;
     
-    for(var i=0; i<seoul.length; i++){
-       if(seoul[i] === 'Kim') {
-           answer = "김서방은 " +i+ "에 있다";
-       }
-    }
-    return answer;
+    var idx = seoul.indexOf('Kim');
+   
+    return "김서방은 " + idx + "에 있다";
+
+    
 }
