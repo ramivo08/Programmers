5adfd1048dcc7249f4a60d8feb6646a230240b63
ramivo08 <hboram8@naver.com>
2023-05-23 01:29:59 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -13,3 +13,9 @@ function solution(phone_number) {
     }
     return answer;
 }
+
+//slice함수는 음수도 지원
+//function hide_numbers(s){
+//    var result = "*".repeat(s.length - 4) + s.slice(-4);
+//return result;
+ // }
