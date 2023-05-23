c74cad101603af7ff4d6781d3b92c2333491f01e
ramivo08 <hboram8@naver.com>
2023-05-23 23:21:08 +0900

    [level 1] Title: 음양 더하기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -2,13 +2,22 @@ function solution(absolutes, signs) {
     var answer = 0;
     
     for(var i=0; i<absolutes.length; i++){
-        if(signs[i] == true){
-            answer = answer+ absolutes[i];
-        }
-        else{
-             answer = answer- absolutes[i];
-        }
-      
-    }
+        signs[i] ? answer+=absolutes[i] : answer-=absolutes[i];
+    }    
     return answer;
+    
 }
+
+
+
+//for(var i=0; i<absolutes.length; i++){
+//        if(signs[i] == true){
+//            answer = answer+ absolutes[i];
+//        }
+//        else{
+ //            answer = answer- absolutes[i];
+//        }
+      
+//    }
+//    return answer;
+//}
