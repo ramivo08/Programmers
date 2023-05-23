1e54440a72a6de27c5aa69f72d107612c611e61f
ramivo08 <hboram8@naver.com>
2023-05-23 23:47:58 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,9 +1,9 @@
 function solution(numbers) {
     var answer = 0;
     
-    for(var i=0; i<10; i++){
-        if(!numbers.includes(i))
-        answer+=i;
+    for(var i=0; i<10; i++){ //9까지 반복해야 하므로 
+        if(!numbers.includes(i)) //i가 numbers에 포함되지 않으면
+        answer+=i;//누적한다
     }
     
     return answer;
