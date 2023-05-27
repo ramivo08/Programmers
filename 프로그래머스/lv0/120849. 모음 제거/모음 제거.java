60781a78ea43a3f316933682c7f3dc29ffc8cdb5
ramivo08 <hboram8@naver.com>
2023-05-28 01:11:32 +0900

    [level 0] Title: 모음 제거, Time: 0.22 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -1,12 +1,23 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = my_string;
         String[] vowels = {"a", "e","i", "o", "e", "u"};
+        //모음을 넣을 배열 vowels 생성
         
-        for(String str : vowels){
-            answer = answer.replaceAll(str ,"");
+         for(String str : vowels){ //vowels을 순회하며 my_string의 문자열을 없앤다
+            my_string = my_string.replaceAll(str ,"");
+             //replaceAll("찾을문자열","바꿀문자열")
         }
         
-        return answer;
+        return my_string;
     }
 }
+
+
+//다른 방법
+//순회하지 않고 바로 리턴
+//class Solution {
+//    public String solution(String my_string) {
+   
+//        return my_string.replaceAll("[aeiou]",""); //[] -정규식을 사용
+//    }
+//}
