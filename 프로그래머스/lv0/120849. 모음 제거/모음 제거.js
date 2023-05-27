f84fc246c33a4af7b18f8953b0da5f82edbc4c7f
ramivo08 <hboram8@naver.com>
2023-05-28 01:24:26 +0900

    [level 0] Title: 모음 제거, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -1,4 +1,6 @@
 function solution(my_string) {
   
-    return my_string.replace(/[aeiou]/g,"");
+    return my_string.replace(/[aeiou]/g,""); //정규식
+   //문법: /pattern/modifiers
+    //modifiers g : 검색패턴을 비교할 때 일치하는 모든 부분을 선택하도록 설정.
 }
