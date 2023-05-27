61daecafd435600f0048f61467d39f6abb322c48
ramivo08 <hboram8@naver.com>
2023-05-27 22:23:50 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -3,8 +3,14 @@ function solution(money) {
         answer.push(Math.floor(money/5500));
     //현재 가진 돈 money와 아메리카노값 5500원을 나누면 나머지값이 나오는데
     //Math.floor 메서드로 소수점 이하는 버린다.
+    //parseIntfmf 사용하여도 된다.
        
         answer.push(money%5500);
     //그 나머지값을 구해본다.
     return answer;
 }
+
+//배열로 바로 리턴하는 방법
+//fucntion solution(money){
+//    return [Math.floor(money / 5500), money % 5500];
+//}
