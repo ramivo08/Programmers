624496ce56ac7904b0edd0b30712c1a1d840d611
ramivo08 <hboram8@naver.com>
2023-05-30 23:45:46 +0900

    [level 0] Title: 배열의 유사도, Time: 0.03 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
@@ -2,11 +2,11 @@ class Solution {
     public int solution(String[] s1, String[] s2) {
         int answer = 0;
         
-        for(int i=0; i<s1.length; i++){
-            for(int j=0; j<s2.length; j++){
-               if( s1[i].equals(s2[j]) ){
-                   answer += 1;
-               }
+        for(String str1 : s1){
+            for(String str2 : s2){
+                if(str1.equals(str2)){
+                    answer++;
+                }
             }
         }
         
