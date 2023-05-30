25c8a37fbdfdfdf0d10ddbd4a0bdb2ad8b7d2a75
ramivo08 <hboram8@naver.com>
2023-05-31 00:05:16 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -3,12 +3,16 @@ function solution(price) {
     
         if(price >= 500000){
             return Math.floor(price - (price*0.2))
+            // return Math.floor(price*0.8) => 위와 같은 결과!
+            //제한사항에 소수점 이하를 버린 정수를 리턴하라고 하여 Math.floor사용
         }else if(price >= 300000){
             return Math.floor(price - (price * 0.1))
+            //(price*0.9)
         }else if(price >= 100000){
            return Math.floor(price - (price * 0.05))
+            //(price*0.95)
         }else{
-            return Math.floor(price)
+            return price
         }
     return answer;
 }
