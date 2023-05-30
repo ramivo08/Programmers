0758b67a70be872eec86caa8a637e0be3774b613
ramivo08 <hboram8@naver.com>
2023-05-31 00:27:43 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -3,13 +3,36 @@ class Solution {
         int answer = 0;
         
         if(price >= 500000){
-            return price *= 0.8;
+            return (int)(price * 0.8);
         }else if(price >= 300000){
-            return price *= 0.9;
+            return (int)(price * 0.9);
         }else if(price >= 100000){
-            return price *= 0.95;
+            return (int)(price * 0.95);
         }
     
         return price;
     }
 }
+
+//자바스크립트로 문제를 풀어보다가 자바풀어보니 int형으로 자료형을 맞춰주는것을 잊어버림!
+//그래서 자스와 똑같이 Math.floor(price*0.8) 이런식으로 실행했더니 에러 발생..
+//error: incompatible types: possible lossy conversion from double to int
+//위와 같이 int로 형변환하니 실행되었다!
+
+
+//하다가 그냥 값을 곱해버리니 실행도 되었다.
+//class Solution {
+//    public int solution(int price) {
+//        int answer = 0;
+        
+//        if(price >= 500000){
+//            return price *= 0.8;
+//        }else if(price >= 300000){
+//            return price *= 0.9;
+//        }else if(price >= 100000){
+//            return price *= 0.95;
+//        }
+    
+//        return price;
+//    }
+//}
