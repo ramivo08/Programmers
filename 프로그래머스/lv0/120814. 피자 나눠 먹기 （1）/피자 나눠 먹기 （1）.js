6b2f26338e46ae6f5b13e4c0b390b2423324c012
ramivo08 <hboram8@naver.com>
2023-06-01 00:35:42 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,12 +1,29 @@
+function solution(n) {
+    var answer = 0;
+    
+    if(n % 7 == 0){
+        return answer = n / 7;
+    }else{
+        return answer = parseInt((n/7)+1);
+    }
+    
+    return answer;
+}
+
+
 
 //Math.ceil 함수를 사용하여 바로 return
 //function solution(n) {
 //    return Math.ceil(n/7);
 //}
 
+// if문을 사용해보니 삼항연산자로도 가능하지 않을까 싶어서 해봤음
+//function solution(n) {
+//    return n%7 == 0 ? n/7 : Math.floor((n/7) +1);
+//}
+
 
 // if문을 사용해보니 삼항연산자로도 가능하지 않을까 싶어서 해봤음
-//실행 ok
-function solution(n) {
-    return n%7 == 0 ? n/7 : Math.floor((n/7) +1);
-}
+//function solution(n) {
+//    return n%7 == 0 ? n/7 : Math.floor((n/7) +1);
+//}
