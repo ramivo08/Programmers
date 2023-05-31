405d345ba96f5e777233af5b2307ec2292eedd79
ramivo08 <hboram8@naver.com>
2023-05-31 23:41:19 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,10 +1,12 @@
 class Solution {
     public int solution(int slice, int n) {
+        //n=사람수, slice=피자조각수
         int answer = 0;
-        if(n % slice == 0){
-            answer = n / slice;
+        if(n % slice == 0){ //n과 slice 나누었을때 나머지가 0이면 
+            answer = n / slice;// n 나누기 slice를 return
         }else{
-            answer = n / slice+1;
+            //n을 slice와 나눴을 때, 나머지가 0이 아니라면
+            answer = n / slice+1; //slice는 n보다 +1을 해준다.
         }
         return answer;
     }
