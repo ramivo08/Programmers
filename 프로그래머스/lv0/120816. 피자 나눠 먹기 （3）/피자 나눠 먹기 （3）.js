fe3c499c90105a2c55fcb67cc163a77e67f79066
ramivo08 <hboram8@naver.com>
2023-05-31 23:54:39 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -1,10 +1,4 @@
 function solution(slice, n) {
-    var answer = 0;
     
-    if(n % slice == 0){
-        return answer =n / slice;
-    }else{
-        return answer = Math.floor(n / slice)+1;
-    }
-    return answer;
+    return Math.ceil(n / slice);
 }
