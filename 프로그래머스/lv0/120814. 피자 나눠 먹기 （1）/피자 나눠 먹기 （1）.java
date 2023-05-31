1a44481886167c54106d1237f77f857ffd542338
ramivo08 <hboram8@naver.com>
2023-06-01 00:40:22 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,13 +1,6 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        
-        if(n % 7 == 0 ){
-            return answer = n / 7;
-        }else{
-            return answer = n/7 +1;
-        }
-        
-        //return answer;
+      
+        return n%7 == 0 ? n/7 : n/7 +1;
     }
 }
