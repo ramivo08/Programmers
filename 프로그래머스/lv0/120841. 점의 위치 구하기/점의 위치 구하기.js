08a0621d1bb4b40376a158f0b74cd23a95aae2e3
ramivo08 <hboram8@naver.com>
2023-06-03 02:13:07 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,6 +1,5 @@
 function solution(dot) {
-    //var answer = 0;
-    
+//x=dot[0] , y=dot[1]
     if(dot[0] > 0 && dot[1] >0){
         return 1;
     }else if(dot[0] < 0 && dot[1] > 0){
@@ -11,6 +10,14 @@ function solution(dot) {
         return 4;
     }
     
-    
-    //return answer;
+}
+
+
+   
+//이중삼항연사자를 사용한 풀이
+function solution(dot) {
+ 
+    return dot[0] > 0? (dot[1] > 0 ? 1 : 4) : (dot[1] > 0 ? 2 : 3);
+    // dot[0] > 0 일때(true), 즉 x좌표가 양수일때 dot[1](y좌표)의 값이 0보다 크면 1사분면, 아니면 4사분면
+    // dot[0] > 0 아닐때(false), x좌표가 음수일때 dot[1](y좌표)의 값이 0보다 작으면 2사분면, 아니면 3사분면 
 }
