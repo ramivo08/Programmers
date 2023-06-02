07167acabc1d5f08b42318dfa6dfaa0d580359cf
ramivo08 <hboram8@naver.com>
2023-06-03 01:46:57 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,16 @@
+function solution(dot) {
+    //var answer = 0;
+    
+    if(dot[0] > 0 && dot[1] >0){
+        return 1;
+    }else if(dot[0] < 0 && dot[1] > 0){
+        return 2;
+    }else if(dot[0] < 0 && dot[1] < 0){
+        return 3;
+    }else {
+        return 4;
+    }
+    
+    
+    //return answer;
+}
