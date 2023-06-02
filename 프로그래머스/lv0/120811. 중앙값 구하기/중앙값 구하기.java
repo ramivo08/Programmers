c0d901098ed59c587f37253a16d6abd4ef347a09
ramivo08 <hboram8@naver.com>
2023-06-03 02:37:19 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.38 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -3,6 +3,6 @@ class Solution {
     public int solution(int[] array) {
     //Arrays.sort = 오름차순 
         Arrays.sort(array);
-        return array[array.length/2];
+        return array[array.length/2]; //해당 인덱스를 배열의 길이 /2 하여 중앙값을 나타낸다.
     }
 }
