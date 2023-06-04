9fdbbbb47b979ae66d3dffba20e5fc28a3300e8e
ramivo08 <hboram8@naver.com>
2023-06-05 00:59:21 +0900

    자릿수 더하기.java

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -3,8 +3,8 @@ class Solution {
         int answer = 0;
         
         while(0 < n){
-            answer += n % 10;
-            n /= 10;
+            answer += n % 10; //n의 각 자리 숫자를 구하기 위해서 10으로 나눈 나머지를 알아낸다.
+            n /= 10; //구한 자리수를 없애기 위해 n / 10으로 나눈다
         }
         return answer;
     }
