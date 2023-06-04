18de48b4a4c3f906d5187d557153c6977648d1c3
ramivo08 <hboram8@naver.com>
2023-06-04 18:43:36 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -0,0 +1,11 @@
+function solution(array, height) {
+    var answer = 0;
+    
+    for(var i=0; i<array.length; i++){
+        if(array[i] > height){
+            answer++;
+        }
+    }
+    
+    return answer;
+}
