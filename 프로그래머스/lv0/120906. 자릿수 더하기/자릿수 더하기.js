cdfdcad92afbe9974b7b185b73f37badd93f3470
ramivo08 <hboram8@naver.com>
2023-06-04 23:51:44 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -3,7 +3,7 @@ function solution(n) {
     
     while(n > 0){
         answer += n%10; //n%10으로 숫자 추출
-         n =Math.floor(n/10);
+         n =Math.floor(n/10);//소수점으로 떨어지므로 정수로 만들어 n에 담는다
     }
     return answer;
 }
