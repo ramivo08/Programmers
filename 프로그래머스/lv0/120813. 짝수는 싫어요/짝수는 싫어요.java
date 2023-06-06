a74b9f8bbcb2f18bc83508c69c456c0a47419937
ramivo08 <hboram8@naver.com>
2023-06-07 01:00:42 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[] solution(int n) {
-        int[] answer = new int[(n+1)/2];
+        int[] answer = new int[(n+1)/2]; //배열의 길이 정해줌. 홀수입력해야 하므로 (n+1)/2
         
         for(var i=1; i<=n; i++){
-            if(i % 2 == 1){
-                answer[i/2] = i;
+            if(i % 2 == 1){ //홀수일때
+                answer[i/2] = i; //홀수값들이 순서대로 들어가게 한다.
             }
         }
         return answer;
