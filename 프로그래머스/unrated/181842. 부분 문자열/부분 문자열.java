9fcc014d197b3ff443bd80feb458247ee29ec5d2
ramivo08 <hboram8@naver.com>
2023-06-09 00:08:21 +0900

    [unrated] Title: 부분 문자열, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.java
+++ 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.java
@@ -1,12 +1,19 @@
 class Solution {
     public int solution(String str1, String str2) {
         
-        if(str2.contains(str1)){
-            return 1;
-        }else{
-            return 0;
-        }
-        
-        
+        return str2.contains(str1) ? 1 : 0 ;
     }
 }
+
+
+//if문 사용
+//class Solution {
+//    public int solution(String str1, String str2) {
+        
+//        if(str2.contains(str1)){
+//            return 1;
+//        }else{
+//            return 0;
+//        }       
+//    }
+//}
