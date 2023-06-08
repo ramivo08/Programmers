d1f2a4031e39c49eaf4b6c77349e777b849a2057
ramivo08 <hboram8@naver.com>
2023-06-09 00:54:15 +0900

    [unrated] Title: 글자 이어 붙여 문자열 만들기, Time: 4.23 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.java
+++ 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.java
@@ -5,7 +5,7 @@ class Solution {
         String answer = "";
         
         String[] str = my_string.split("");
-        System.out.println(Arrays.toString(str));
+        System.out.println(Arrays.toString(str));//z, p, i, a
         //System.out.println(str.toString); 했더니 주소값?이 나왔었다.
 
         for(int i=0; i<index_list.length; i++){
