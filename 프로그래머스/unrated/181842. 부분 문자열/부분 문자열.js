14d116a8ca7e83376d386e978788416df2c49f4c
ramivo08 <hboram8@naver.com>
2023-06-08 23:42:57 +0900

    [unrated] Title: 부분 문자열, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.js
+++ 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.js
@@ -1,8 +1,16 @@
+//if문으로 조건을 주어 풀어보니 삼항연산자로도 풀이 가능할거 같아서 작성해보니 실행되었다.
 function solution(str1, str2) {
-    var answer = 0;
-    
-    if(str2.includes(str1)){
-        answer=1;
-    }
-    return answer;
+
+    return str2.includes(str1) ? 1 : 0;
 }
+
+
+//처음에 풀었던 것.
+//function solution(str1, str2) {
+//    var answer = 0;
+    
+//    if(str2.includes(str1)){
+//        answer=1;
+//    }
+//    return answer;
+//}
