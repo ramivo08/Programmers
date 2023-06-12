bada1e1acd29325da05df64deabf15dd0f24ea43
ramivo08 <hboram8@naver.com>
2023-06-12 22:46:08 +0900

    [unrated] Title: rny_string, Time: 1.45 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181863. rny＿string/rny＿string.java
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.java
@@ -1,6 +1,15 @@
 class Solution {
     public String solution(String rny_string) {
-           
-        return rny_string.replaceAll("m","rn");
+        String str="";
+        String arr[] = rny_string.split("");
+        
+        for(int i=0; i<arr.length; i++){
+            if(arr[i].equals("m")){
+                str += "rn";
+            }else{
+                str += arr[i];
+            }
+        }
+        return str;
     }
 }
