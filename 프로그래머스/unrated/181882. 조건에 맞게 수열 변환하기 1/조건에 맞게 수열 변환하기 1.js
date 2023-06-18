333f20ccfceed02636b17122241abc3fa6c709c5
ramivo08 <hboram8@naver.com>
2023-06-19 00:14:57 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 8.75 ms, Memory: 45.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -1,8 +1,23 @@
 //map 사용 - 다른사람 풀이 참조
-function solution(arr) {
-    return arr.map(a => {
-        if(a >= 50 && a %2 ===0) return Math.floor(a/2)
-        if(a < 50 && a %2 === 1) return a*2
-        return a
-    })
+//function solution(arr) {
+//    return arr.map(a => {
+//        if(a >= 50 && a %2 ===0) return Math.floor(a/2)
+//        if(a < 50 && a %2 === 1) return a*2
+//        return a
+//    })
+//}
+
+function solution(arr){
+    var answer = [];
+    
+    for(var i=0; i<arr.length; i++){
+        if(arr[i] < 50 && arr[i] % 2 != 0){
+            answer.push(arr[i]*2)
+        }else if(arr[i] >= 50 && arr[i] % 2 == 0){
+            answer.push(arr[i]/2)
+        }else{
+            answer.push(arr[i])
+        }
+    }
+    return answer;
 }
