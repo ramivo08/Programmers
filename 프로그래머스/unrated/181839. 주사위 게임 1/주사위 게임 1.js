cb42d4a4d229364fb00b8006330e0a17a60d67de
ramivo08 <hboram8@naver.com>
2023-06-22 00:33:24 +0900

    주사위 게임 1.js

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -3,10 +3,12 @@ function solution(a, b) {
     
     if(a%2 != 0 && b%2 !=0){
         answer = (a*a)+(b*b)
+        //제곱을 구할 때, a** 하면 제곱이 된다. (a*a)==a**2
     }else if(a%2 != 0 || b%2 !=0){
         answer = 2*(a+b)
     }else{
         answer = Math.abs(a-b)
+        //Math.abs() : 주어진 숫자의 절대값을 반환
         //a-b만 했을때, 실패했는데 Math.abs()메서드를 사용해서 절대값으로 풀이
     }
     return answer;
