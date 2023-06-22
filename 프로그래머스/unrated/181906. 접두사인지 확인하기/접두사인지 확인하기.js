6ddcb60aff4ccfe9590d8cf8ced1997a4e749f19
ramivo08 <hboram8@naver.com>
2023-06-23 00:54:39 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,8 +1,16 @@
-function solution(my_string, is_prefix) {
-    
+//삼항연산자로 풀이
+//function solution(my_string, is_prefix) {
+
+//     return my_string.indexOf(is_prefix) == 0? 1: 0;
+//}
+
+
+
+
+function solution(my_string, is_prefix) {    
     for(var i=0; i<is_prefix.length; i++){
         if(my_string[i] != is_prefix[i])
             return 0;
     }
-     return 1;
+    return 1;
 }
