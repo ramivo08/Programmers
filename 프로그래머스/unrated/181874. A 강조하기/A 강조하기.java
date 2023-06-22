1b55737580d19a15e6297e54afa1cf4360060363
ramivo08 <hboram8@naver.com>
2023-06-22 23:37:41 +0900

    [unrated] Title: A 강조하기, Time: 0.06 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -1,9 +1,18 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        
-        myString = myString.toLowerCase();
-        answer = myString.replaceAll("a","A");
-        return answer;
+       
+        return myString.toLowerCase().replaceAll("a","A");
     }
 }
+
+
+//처음풀이
+//class Solution {
+//    public String solution(String myString) {
+//        String answer = "";
+        
+//        myString = myString.toLowerCase();
+//        answer = myString.replaceAll("a","A");
+//        return answer;
+//    }
+//}
