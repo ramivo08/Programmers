3cb0ff205f0cc14c22a384e3eb6935da150ba5ae
ramivo08 <hboram8@naver.com>
2023-06-25 22:52:00 +0900

    배열의 원소만큼 추가하기.js

--- 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.js
+++ 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.js
@@ -2,7 +2,7 @@ function solution(arr) {
     var answer = [];
     
     for(var i=0; i<arr.length; i++){
-        for(var j=0; j<arr[i]; j++){
+        for(var j=0; j<arr[i]; j++){ //arr[i]만큼 반복해서 배열answer에 넣는다
             answer.push(arr[i]);
         }
     }
