3bb3326ddd1ba91dc3d28d89889aff6802d0fbdf
ramivo08 <hboram8@naver.com>
2023-06-27 23:53:24 +0900

    [unrated] Title: 홀짝 구분하기, Time: 42.02 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.js
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.js
@@ -10,15 +10,20 @@ rl.on('line', function (line) {
     input = line.split(' ');
 }).on('close', function () {
     n = Number(input[0]);
+  
+    //한줄로 풀이
+    n%2==0 ? console.log(n+" is even") : console.log(n+" is odd")
+
+    
+    //첫풀이
+   // var str='';
    
-    var str='';
-   
-    if(n%2==0){
-        str = 'even';
-    }else{
-         str = 'odd';   
-    }
+   // if(n%2==0){
+   //     str = 'even';
+   // }else{
+   //      str = 'odd';   
+   // }
     
    // console.log(n,'is',str);
-    console.log(`${n} is ${str}`);
+   // console.log(`${n} is ${str}`);
 });
