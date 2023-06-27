c6df01baf0442ddde0d429f6800bc2abf7bb5b16
ramivo08 <hboram8@naver.com>
2023-06-28 00:26:41 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.05 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+       int answer =0;
+        for(int i=0; i<my_string.length(); i++){
+           String str = my_string.substring(i, my_string.length());
+           if(str.equals(is_suffix)) 
+               answer= 1;
+    }
+       return answer;
+    }
+}
