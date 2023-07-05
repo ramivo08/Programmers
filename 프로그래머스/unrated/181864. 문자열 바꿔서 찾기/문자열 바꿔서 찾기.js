b7fa38a1de1717c6fec14efe3b4ed84729fe4690
ramivo08 <hboram8@naver.com>
2023-07-06 00:11:45 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 2.69 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -1,20 +1,43 @@
+//풀이2
 function solution(myString, pat) {
     var answer = 0;
-    var str = myString.split("");
-    var str2 ="";
+    var str ="";
    
-    for(var i=0; i<str.length; i++){
-        if(str[i] === "A"){
-            str[i] = "B"
-        }else if(str[i] === "B"){
-            str[i] = "A"
+    for(var i=0; i<myString.length; i++){
+        if(myString[i] === "A"){
+            str += "B";
+        }else{
+            str += "A";
         }
-        str2 += str[i];
-        console.log('str2',str2);
+      
+        console.log('str',str);
     }
     
-    if(str2.includes(pat)){
+    if(str.includes(pat)){
         answer=1;
     }
     return answer;
 }
+
+
+//풀이1
+//function solution(myString, pat) {
+//    var answer = 0;
+//    var str = myString.split("");
+//    var str2 ="";
+   
+//    for(var i=0; i<str.length; i++){
+//        if(str[i] === "A"){
+//            str[i] = "B"
+//        }else if(str[i] === "B"){
+//            str[i] = "A"
+//        }
+//        str2 += str[i];
+//        console.log('str2',str2);
+//    }
+    
+//    if(str2.includes(pat)){
+//        answer=1;
+//    }
+//    return answer;
+//}
