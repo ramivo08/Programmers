a7c4be183d5c0d1bc74b1d532b18f9b30cad3fdd
ramivo08 <hboram8@naver.com>
2023-07-06 00:18:35 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 2.00 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/README.md
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 문자열 바꿔서 찾기 - 181864 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181864) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181864?language=java) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 2.69 ms
+메모리: 74 MB, 시간: 2.00 ms
 
 ### 구분
 
---
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        String str = "";
+        String[] ms = myString.split("");
+        
+        for(int i=0; i<ms.length; i++){
+            if(ms[i].equals("A")){
+                str += "B";
+            }else{
+                str += "A";
+            }
+        }
+        
+        //if문 사용
+        //if(str.contains(pat)){
+        //    answer =1;
+        //}
+        //return answer;
+       
+        //삼항연산자 사용
+        return str.contains(pat) ? 1 : 0;
+    }
+}
