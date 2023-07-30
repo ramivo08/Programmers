769db1b398cd16833105abaa87c43112cb695b27
ramivo08 <hboram8@naver.com>
2023-07-30 23:35:11 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.04 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,15 +1,46 @@
 import java.util.*;
-class Solution {
-    public List solution(String my_string) {
-        
+
+class Solution{
+    public String[] solution(String my_string){
         List<String> list = new ArrayList<>();
+        String[] arr = my_string.split(" ");
         
-        String[] myString = my_string.split(" ");
-        for(int i=0; i<myString.length; i++){
-            if(!myString[i].equals("")){
-                list.add(myString[i]);
-            }
+        //for문 사용
+        //for (int i=0; i<arr.length; i++){
+        //    if(!arr[i].equals("")){
+        //        list.add(arr[i]);
+        //    }
+        //}
+        
+        //확장for문 사용
+        for(String str : arr){
+            if(!str.equals(""))
+            list.add(str);
         }
-        return list;
+        
+        String[] answer = new String[list.size()];
+//        for(int i=0; i<list.size(); i++){
+//            answer[i] = list.get(i);
+//        }
+        for(int i=0; i<answer.length; i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
     }
 }
+
+
+//class Solution {
+//    public List solution(String my_string) {
+        
+//        List<String> list = new ArrayList<>();
+        
+//        String[] myString = my_string.split(" ");
+//        for(int i=0; i<myString.length; i++){
+//            if(!myString[i].equals("")){
+//                list.add(myString[i]);
+//            }
+//        }
+//        return list;
+//    }
+//}
