d8592d444bc231a52b4a10e843fef84e0d7fa110
ramivo08 <hboram8@naver.com>
2023-07-30 23:09:59 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -1,4 +1,16 @@
 function solution(my_string) {
-   
-    return my_string.trim().split(/\s+/);
+    var answer =[]
+    var arr = my_string.split(" ");
+    for(var i=0; i<arr.length; i++){
+        if(arr[i] !== ""){
+            answer.push(arr[i])
+        }
+    }
+    return answer
+
 }
+
+//function solution(my_string) {
+        //trim과 정규식 사용한 풀이
+//    return my_string.trim().split(/\s+/);
+//}
