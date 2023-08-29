c8fdc73b0c336d9b1319cea0aa551a5e97d0a003
ramivo08 <hboram8@naver.com>
2023-08-30 00:02:47 +0900

    [unrated] Title: 5명씩, Time: 0.04 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] names) {
+       
+        List<String> list = new ArrayList<>();
+        
+        for(int i=0; i<names.length; i+=5){
+            list.add(names[i]);
+        }
+        return list.toArray(new String[0]);
+    }
+}
