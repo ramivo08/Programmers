a4fe00f63a66881fc03542c632fae60cb0d7d3da
ramivo08 <hboram8@naver.com>
2023-08-30 00:33:32 +0900

    [unrated] Title: 5명씩, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.java
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -9,5 +9,7 @@ class Solution {
             list.add(names[i]);
         }
         return list.toArray(new String[0]);
+        //list를 배열로 변환하기 위해 toArray() 메서드를 사용
+        //파라미터로 변환할 배열의 타입과 같은 객체를 생성하여 넣어준다.
     }
 }
